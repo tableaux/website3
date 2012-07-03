10b9daf1bf28956c04b8ff8b63e224901934717f
Bram van der Kruis <bram@tableaux.nl>
2012-07-03 10:49:48 +0200

    changes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "website 3 update 6 van erik";
 //mail("bram@tableaux.nl","mijn onderwerp","mijn body");
 echo "test";
-echo "testbranch";
+echo "testbranch<br />";
 echo "hotfix3";
 //    phpinfo();
 ?>
